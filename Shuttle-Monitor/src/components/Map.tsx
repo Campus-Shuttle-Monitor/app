27365786fc721bddd39a046e6ae6ffab2336e6c3
Jamason <jsvg78@umsystem.edu>
2021-03-12 13:00:30 -0600

    Fixed formatting on various files. Added homepage updates with shuttle hours and adjusted size and position of map. Added background image. Refactored project to remove unneccessary screens folder. Removed unneccessary navigation.

--- Shuttle-Monitor/src/components/Map.tsx
+++ Shuttle-Monitor/src/components/Map.tsx
@@ -1,30 +1,31 @@
- import React from 'react'
-    import MapView from 'react-native-maps'
-    import { StyleSheet, Dimensions } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps'
+import { StyleSheet, Dimensions } from 'react-native'
 
-    const height = Dimensions.get('window').height
+const height = Dimensions.get('window').height * 0.6
+const width = Dimensions.get('window').width * 0.85
 
-    const Map = () => {
-      return (
-        <MapView
-          style={styles.map}
-          loadingEnabled={true}
-          region={{
-            latitude: 37.9485,
-            longitude: -91.77154,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121
-          }}
-        >
+const styles = StyleSheet.create({
+  map: {
+    height,
+    width,
+    borderRadius: 15
+  }
+})
 
-        </MapView>
-      )
-    }
+const Map = () => {
+  return (
+    <MapView
+      style={styles.map}
+      loadingEnabled={true}
+      region={{
+        latitude: 37.9485,
+        longitude: -91.77154,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121
+      }}
+    ></MapView>
+  )
+}
 
-    const styles = StyleSheet.create({
-      map: {
-        height
-      }
-    })
-
-    export default Map
+export default Map
