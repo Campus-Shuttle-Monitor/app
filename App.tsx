a36a7b7823685456f4b46aae3c3f89c775622901
Mitchell Skaggs <skaggsm333@gmail.com>
2021-03-17 01:45:50 -0500

    Add eslint and format everywith with Intellij's default

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import {MapScreen} from './src/screens/MapScreen'
 
-export default () => {
-  return (
-    <MapScreen></MapScreen>
-  );
-}
+export default (): JSX.Element => (
+    <MapScreen/>
+);
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,31 +1,27 @@
 import React from 'react'
 import MapView from 'react-native-maps'
-import { StyleSheet, Dimensions } from 'react-native'
+import {Dimensions, StyleSheet} from 'react-native'
 
 const height = Dimensions.get('window').height * 0.6
 const width = Dimensions.get('window').width * 0.85
 
 const styles = StyleSheet.create({
-  map: {
-    height,
-    width,
-    borderRadius: 15
-  }
+    map: {
+        height,
+        width,
+        borderRadius: 15
+    }
 })
 
-const Map = () => {
-  return (
+export const Map = (): JSX.Element => (
     <MapView
-      style={styles.map}
-      loadingEnabled={true}
-      region={{
-        latitude: 37.9485,
-        longitude: -91.77154,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121
-      }}
-    ></MapView>
-  )
-}
-
-export default Map
+        style={styles.map}
+        loadingEnabled={true}
+        region={{
+            latitude: 37.9485,
+            longitude: -91.77154,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+        }}
+    />
+);
